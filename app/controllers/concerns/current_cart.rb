49090e2eca75868e75cebc727d66da73242f027e
divo <sdiviney@zendesk.com>
2018-08-26 18:17:10 +0000

    Logic to add LineItem to current cart

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -5,7 +5,7 @@ module CurrentCart
       @cart = Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
       @cart = Cart.create
-      session[:cart_id] = @card.id
+      session[:cart_id] = @cart.id
     end
     
 end
