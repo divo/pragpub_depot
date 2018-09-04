ae9a5aa3eafa840ea8bc15472cb1197535c12709
divo <sdiviney@zendesk.com>
2018-09-04 16:09:57 +0000

    Added down() to combine_line_items migration

--- db/migrate/20180904132219_combine_items_in_cart.rb
+++ db/migrate/20180904132219_combine_items_in_cart.rb
@@ -21,4 +21,21 @@ class CombineItemsInCart < ActiveRecord::Migration[5.2]
   end
   
   
+  def down
+    # split items where quantity > 1 into multiple items
+    LineItem.where("quantity>1").each do |line_item|
+      # add individual items
+      line_item.quantity.times do
+        LineItem.create(
+          cart_id: line_item.cart_id,
+          product_id: line_item.product_id,
+          quantity: 1
+        )
+      end
+      
+      # remove original
+      line_item.destroy
+    end
+  end
+  
 end
