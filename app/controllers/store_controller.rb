83582ec804f7247aac0d6087cf04695791dd014d
divo <sdiviney@zendesk.com>
2018-09-05 13:18:52 +0000

    Adding Ajax to cart
    Moved line_item to a template and updated cart to use it
    Moved cart to a template and modified it to used a cart passed as a parameter, not from the session.
    Modified cart.show to use the template so it still works when JS is disabled.
    Updated the application.html.erb to render the cart in the sidebar.
    Modified the StoreController to set/setup the cart using the cart concern.
    Redirecting line_item.create success to store_index_url to refresh the page whenever an item is added to cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,6 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
   def index
     @products = Product.order(:title) #Pretty sure scaffold created a Product accessor for db?
   end
