4cc2a1f8982cd6ecf370fdb385a1eed4ab7ace3c
divo <sdiviney@zendesk.com>
2018-09-06 10:50:33 +0000

    Capturing order details
    Defining optional belongs_to relationship between LineItem and Order
    Order has_many dependent LineItem
    Order.create creates a new order, adds all the LineItems from the current cart, saves the order to DB, then destroys the cart and forwards to the main store page

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,19 @@
 class Order < ApplicationRecord
-  validates :name, :address, :email, presence: true
-  validates :pay_type, inclusion: pay_types.keys
   enum pay_type: {
     "Check" => 0,
     "Credit Card" => 1,
     "Purchase order" => 2
   }
+  
+  has_many :line_items, dependent: :destroy
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
+  
+  
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil # Disassociate from Cart so LineItem isnt destroyed with the Cart
+      line_items << item # Add the item to the line_items collection. Rails uses the belongs_to and has_many declartion in the respecitve models to knit them together in the db
+    end
+  end
 end
