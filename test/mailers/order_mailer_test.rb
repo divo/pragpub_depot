e940dd6f65a708b18fb655e8ed655d7f18d4420c
divo <sdiviney@zendesk.com>
2018-09-10 20:13:33 +0000

    Mailer tests

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -1,20 +1,18 @@
 require 'test_helper'
 
 class OrderMailerTest < ActionMailer::TestCase
-  test "received" do
-    mail = OrderMailer.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+   test "received" do
+    mail = OrderMailer.received(orders(:one))
+    assert_equal "Store order confirmation", mail.subject
+    assert_equal ["dave@example.com"], mail.to
+    assert_equal ["test@example.com"], mail.from
   end
 
   test "shipped" do
-    mail = OrderMailer.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderMailer.shipped(orders(:one))
+    assert_equal "Store order shipped!", mail.subject
+    assert_equal ["dave@example.com"], mail.to
+    assert_equal ["test@example.com"], mail.from
   end
 
 end
