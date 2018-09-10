70f399c7b8ce2835358cd84898941122c480ae0e
divo <sdiviney@zendesk.com>
2018-09-10 19:46:32 +0000

    Order templates

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,25 @@
+class OrderMailer < ApplicationMailer
+  default from: 'Mr Test <test@example.com>'
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.received.subject
+  #
+  def received
+    @order = order
+
+    mail to: order.email, subject: 'Store order confirmation'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.shipped.subject
+  #
+  def shipped
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
