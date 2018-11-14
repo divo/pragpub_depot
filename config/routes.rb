80275840384e4f754590d14a330fb40c06cb6ed6
Steven Diviney <sdiviney@zendesk.com>
2018-11-14 13:26:16 +0000

    Added a 'locale' scope to the routes.
    Only put certain pages behind it.
    Added logic to ApplicationController to pull that locale param out and
    set it on the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+
   get 'admin' => 'admin#index'
   controller :sessions do
     get 'login' => :new
@@ -6,17 +8,19 @@ Rails.application.routes.draw do
     delete 'logout' => :destroy
   end
 
-  get 'admin/index'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destory'
+#  get 'admin/index' #What are these doing here again?
+#  get 'sessions/new'
+#  get 'sessions/create'
+#  get 'sessions/destory'
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
   resources :products do
     get :who_bought, on: :member
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
 end
